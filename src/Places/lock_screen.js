d51f76d5fd41cfccaf02660433fe0f9e8017c589
LeeFCash <leecash133@gmail.com>
2025-03-15 19:04:29 -0400

    made some UI for login_page

--- src/Places/lock_screen.js
+++ src/Places/lock_screen.js
@@ -1,8 +1,11 @@
-//import { Link } from 'react-router-dom';
 export function LOCK_SCREEN() {
-return (<div>
-        <main>
-	<p>testing does this work</p>
+return (<div className='login_div'>
+        <main className='login_main'>
+	 <div className='login_window'>
+	  <img className='login_IMG' src={require('.././IMG/meG.jpg')} alt='404'></img>
+	  <p className='login_text'>type password for user LeeCash</p>
+	  <input></input>
+	 </div>
 	</main>
 </div>)}
 export default LOCK_SCREEN;
